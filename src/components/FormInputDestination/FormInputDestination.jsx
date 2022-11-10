ca59c636e4e55bd172a9fbc286cb79eb2ad9cf27
Evan Bond <>
2022-11-10 18:43:55 +0300

    Create FormInputDestination

---
+++ src/components/FormInputDestination/FormInputDestination.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+// import DestinationPoint from '../DestinationPoint/DestinationPoint';
+import './FormInputDestination.css';
+
+// function getCenterOfMap() {}
+
+const FormInputDestination = () => {
+  const [mapPoint, setMapPoint] = useState('');
+
+  return (
+    <div className='map-routes'>
+      <div>
+        <form>
+          <label>
+            <input
+              type='text'
+              value={mapPoint}
+              placeholder='Новая точка маршрута'
+              onChange={(e) => setMapPoint(e.target.value)}
+            />
+          </label>
+        </form>
+      </div>
+      {/* {mapPoint && (
+        <ul>
+          <DestinationPoint mapValue={mapPoint} />
+        </ul>
+      )} */}
+    </div>
+  );
+};
+
+export default FormInputDestination;
